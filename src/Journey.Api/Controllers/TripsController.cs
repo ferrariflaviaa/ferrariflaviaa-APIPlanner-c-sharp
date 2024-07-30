c66d4e0408bfbafa62afa5a69a3a230840dade17
ferrariflaviaa <flaviaferrarit@gmail.com>
2024-07-30 20:26:29 -0300

    feat: rotas para criação e busca de viagens

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,40 @@
+﻿using Journey.Application.UseCases.Trips.GetAll;
+using Journey.Application.UseCases.Trips.Register;
+using Journey.Communication.Requests;
+using Journey.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class TripsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterTripJson resquest)
+    {
+        try
+        {
+            var useCase = new RegisterTripUseCase();
+            var response = useCase.Execute(resquest);
+
+            return Created(string.Empty, response);
+        }
+        catch(JourneyException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch 
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Erro desconehcido");
+        }
+    }
+
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var userCase = new GetAllTripUseCase();
+        var response = userCase.Execute();
+        return Ok(response);
+    }
+}
---
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Journey.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Infrastructure;
+public class JourneyDbContext : DbContext
+{
+    public DbSet<Trip> Trips { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=C:\\Users\\flavia\\Desktop\\Projetos\\APIPlanner-c-sharp\\Journey\\JourneyDatabase.db");
+    }
+}
